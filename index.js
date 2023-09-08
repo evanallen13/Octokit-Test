385516d1a07cfa5157b661cf938e7afa44938591
Evan Allen <eallen@xpirit.com>
2023-09-08 10:56:58 -0400

    test env var

--- index.js
+++ index.js
@@ -1,106 +1,91 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import { getEnvironmentVariable } from "./src/environment.js";
 
-const name = process.env.NAME.replace(/\s/g, '_')
-const value = process.env.VALUE
-const token = process.env.PAT_TOKEN
-const octokit = github.getOctokit(token);
+// const name = process.env.NAME.replace(/\s/g, '_')
+// const value = process.env.VALUE
+// const token = process.env.PAT_TOKEN
+// const octokit = github.getOctokit(token);
 
-const repoName = process.env.REPO;
-const ownerName = process.env.OWNER;
+// const repoName = process.env.REPO;
+// const ownerName = process.env.OWNER;
 
-const createVariable = (value) => {
+// const createVariable = (value) => {
 
-    let url = 'POST '
-    url += "/repos/" + repoName
-    url += '/actions/variables'
+//     let url = 'POST '
+//     url += "/repos/" + repoName
+//     url += '/actions/variables'
 
-    return octokit.request(url, {
-        owner: ownerName,
-        repo: repoName,
-        name: name,
-        value: value
-    })
-}
+//     return octokit.request(url, {
+//         owner: ownerName,
+//         repo: repoName,
+//         name: name,
+//         value: value
+//     })
+// }
 
-const setVariable = (value) => {
+// const setVariable = (value) => {
 
-    let url = 'PATCH '
-    url += "/repos/" + repoName
-    url += '/actions/variables/' + name
+//     let url = 'PATCH '
+//     url += "/repos/" + repoName
+//     url += '/actions/variables/' + name
 
-    return octokit.request(url, {
-        owner: ownerName,
-        repo: repoName,
-        name: name,
-        value: value
-    })
-}
+//     return octokit.request(url, {
+//         owner: ownerName,
+//         repo: repoName,
+//         name: name,
+//         value: value
+//     })
+// }
 
-const getVariable = (varname) => {
+// const getVariable = (varname) => {
 
-    let url = 'GET '
-    url += "/repos/" + repoName
-    url += '/actions/variables/' + varname
-    console.log(url)
+//     let url = 'GET '
+//     url += "/repos/" + repoName
+//     url += '/actions/variables/' + varname
+//     console.log(url)
 
-    return octokit.request(url, {
-        owner: ownerName,
-        repo: repoName,
-        name: name
-    })
-}
+//     return octokit.request(url, {
+//         owner: ownerName,
+//         repo: repoName,
+//         name: name
+//     })
+// }
 
-const exists = async () => {
-    let exists = false;
+// const exists = async () => {
+//     let exists = false;
 
-    try {
-        const response = await getVariable(name)
-        exists = (response.status === 200) ? true : false
-    } catch (error) {
-        exists = false
-    }
+//     try {
+//         const response = await getVariable(name)
+//         exists = (response.status === 200) ? true : false
+//     } catch (error) {
+//         exists = false
+//     }
 
-    return exists
-}
+//     return exists
+// }
 
 
-const increment = async () => {
-    const doesExist = await exists()
+// const increment = async () => {
+//     const doesExist = await exists()
 
-    if (doesExist) {
-        let variable = await getVariable(name)
-        variable = variable.data.value
-        if (variable.match(/^[0-9]+$/)) {
-            variable = (parseInt(variable) + 1).toString()
-            setVariable(variable)
-        }
-    }
-    else {
-        createVariable((1).toString())
-    }
-}
+//     if (doesExist) {
+//         let variable = await getVariable(name)
+//         variable = variable.data.value
+//         if (variable.match(/^[0-9]+$/)) {
+//             variable = (parseInt(variable) + 1).toString()
+//             setVariable(variable)
+//         }
+//     }
+//     else {
+//         createVariable((1).toString())
+//     }
+// }
 
-const getEnv = async (varname) => {
-    const response = await octokit.rest.actions.getEnvironmentSecret({
-        repository_id: "688581388",
-        environment_name: "main", 
-        secret_name: "HELLO", 
-    });
-
-    // url : 'https://api.github.com/repositories/evanallen13%2FOctokit-Test/environments/main/secrets/hello'
-    // url : /repos/evanallen13/Octokit-Test/actions/variables/VERSION
-    // let url = 'GET '
-    // url += "/repos/" + repoName
-    // url += "/environments/main/secrets/hello"
-
-    // return octokit.request(url, {
-    //     owner: ownerName,
-    //     repo: repoName,
-    //     name: name
-    // })
-}
 
 
 // increment()
-getEnv("VERSION")
+const run = async () => {
+    const response = await getEnvironmentVariable("HELLO")
+    console.log(response)
+}
+
+run()
