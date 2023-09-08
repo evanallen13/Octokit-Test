6bc94054be40fa6139331a5bcd7efccf7777a749
Evan Allen <eallen@xpirit.com>
2023-09-08 14:35:06 -0400

    test env var

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ const run = async () => {
     const ev = new EnvironmentVariable();
     // const response = await ev.getEnvironmentVariable()
     // console.log(response.data)
-    
     ev.incrementEnvironmentVariable()
 }
 
