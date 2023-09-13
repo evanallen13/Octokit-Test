6a5a40013e57fb183f713c85b8060b5169fe426a
Evan Allen <eallen@xpirit.com>
2023-09-13 12:34:22 -0400

    v1001

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { EnvironmentVariable } from "./src/environmentVariable.js";
 
-
 const run = async () => {
     const environmentVariable = new EnvironmentVariable()
     await environmentVariable.setVariableInAllEnvironments()
